ca56b34d8655db2dfa1780adade2408f03b8f4e9
Mamatha <24b01a1201@svecw.edu.in>
2026-03-05 09:48:31 +0530

    change in settings.py

--- task/settings.py
+++ task/settings.py
@@ -123,5 +123,5 @@ EMAIL_HOST_PASSWORD='ymok ulnz oace umxr'
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =(BASE_DIR, 'static')
 
